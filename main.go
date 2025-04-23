62f5e3eb5c61e1c2be2d99834ed646116095c079
gustavsant <gustavosant3301@gmail.com>
2025-04-23 12:42:31 -0300

    minor changes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gustavsant/go-crud/config"
 	"github.com/gustavsant/go-crud/routes"
@@ -10,6 +11,10 @@ func main() {
 
 	config.ConnectDatabase()
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(cors.New(config))
 
 	router.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
