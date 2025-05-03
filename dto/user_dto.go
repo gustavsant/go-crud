d64eaccfc1eb147d2a0aa9ec1ea0f72754abe21e
gustavsant <gustavosant3301@gmail.com>
2025-05-02 21:01:59 -0300

    added email validation and password min characters (literally 1 line of code)

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type RegisterUserDTO struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Email    string             `json:"email" binding:"required"`
-	Password string             `json:"password" binding:"required"`
+	Email    string             `json:"email" binding:"required,email"`
+	Password string             `json:"password" binding:"required,min=8"`
 }
 
 type AuthenticateUserDTO struct {
